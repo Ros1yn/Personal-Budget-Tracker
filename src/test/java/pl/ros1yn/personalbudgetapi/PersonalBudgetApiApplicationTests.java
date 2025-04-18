c4d00b4d77893ca9f9e55b8e1e28cafcc5e2bc33
Mateusz <mk.kwiatkowski30@gmail.com>
2025-04-18 16:19:49 +0200

    First commit

---
+++ src/test/java/pl/ros1yn/personalbudgetapi/PersonalBudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ros1yn.personalbudgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
